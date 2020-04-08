40f684e1d5c080bbd97caafbb66f34f75d57837c
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-08 11:06:11 -0500

    Indentado del código y escritura de comentarios

--- ConNombre/Escritor.c
+++ ConNombre/Escritor.c
@@ -1,4 +1,10 @@
+/*
+	Para la implementación de una comunicación bidireccional haremos uso de
+	dos tuberías, la primera enviara un mensaje del proceso Escritor al
+	proceso Lector y la segunda del proceso Lector al proceso Escritor.
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -10,35 +16,57 @@ int main(void){
   char mensajeEnviado[TAM];
   char mensajeRecibido[TAM];
 
-  /*Creación de la tubería*/
-  if(mkfifo("escritor", S_IWUSR |
-                        S_IRUSR |
-                        S_IRGRP |
-                        S_IROTH
+  /*Creación de las tuberías*/
+  if(mkfifo("escritor", S_IWUSR | /*El usuario puede escribir*/
+                        S_IRUSR | /*El usuario puede leer*/
+                        S_IRGRP | /*El grupo puede leer*/
+                        S_IROTH   /*Otros pueden leer*/
   )!=0){
-    printf("Hubo un problema al crear la tuberia del escritor.\n");
+    printf("Hubo un problema al crear la tubería del escritor.\n");
   }
   if(mkfifo("lector", S_IWUSR |
                       S_IRUSR |
                       S_IRGRP |
                       S_IROTH
   )!=0){
-    printf("Hubo un problema al crear la tuberia del lector.\n");
+    printf("Hubo un problema al crear la tubería del lector.\n");
   }
 
-  tuberiaEscritor=fopen("escritor","w");
-  tuberiaLector=fopen("lector","r");
+  /*Abrimos las tuberías como si fueran un fichero*/
+  tuberiaEscritor=fopen("escritor", "w");
+  tuberiaLector=fopen("lector", "r");
 
+  if(tuberiaEscritor==NULL || tuberiaLector==NULL){
+    printf("No se pudo abrir las tuberías.\n");
+    exit(0);
+  }
+
+  /*
+    El proceso Escritor se encarga de leer un mensaje introducido
+    por medio del teclado y acto seguido escribirlo en la
+    tubería, para que lo reciba el proceso Lector. Después espera una
+    respuesta por parte del proceso Lector por medio de la otra tubería.
+    Al escribir el mensaje "Adiós\n" acaban los dos procesos.
+  */
   while(printf("Escritor: ") != 0 &&
-    fgets(mensajeEnviado,TAM, stdin) != NULL &&
-    fputs(mensajeEnviado, tuberiaEscritor) > 0 &&
-    strcmp(mensajeEnviado, "Adios\n") !=0){
+        fgets(mensajeEnviado, TAM, stdin) != NULL &&
+        fputs(mensajeEnviado, tuberiaEscritor) > 0 &&
+        strcmp(mensajeEnviado, "Adiós\n") !=0){
+      /*
+        Cerramos la tubería para indicar que terminamos de escribir y
+        después la abrimos para volver a escribir en ella.
+      */
       fclose(tuberiaEscritor);
-      tuberiaEscritor=fopen("escritor","w");
+      tuberiaEscritor=fopen("escritor", "w");
+      /*
+        Obtenemos el mensaje escrito por el proceso Lector y lo mostramos
+        a pantalla.
+      */
       fgets(mensajeRecibido, TAM, tuberiaLector);
       printf("Lector: %s\n", mensajeRecibido);
     }
 
+  /*Cerramos la tuberías*/
   fclose(tuberiaEscritor);
   fclose(tuberiaLector);
 }
--- ConNombre/Lector.c
+++ ConNombre/Lector.c
@@ -1,4 +1,10 @@
+/*
+	Para la implementación de una comunicación bidireccional haremos uso de
+	dos tuberías, la primera enviara un mensaje del proceso Escritor al
+	proceso Lector y la segunda del proceso Lector al proceso Escritor.
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -10,19 +16,42 @@ int main(void){
   char mensajeEnviado[TAM];
   char mensajeRecibido[TAM];
 
-  tuberiaEscritor=fopen("escritor","r");
-  tuberiaLector=fopen("lector","w");
+  /*Abrimos las tuberías como si fueran un fichero*/
+  tuberiaEscritor=fopen("escritor", "r");
+  tuberiaLector=fopen("lector", "w");
 
-  while (fgets(mensajeRecibido, TAM, tuberiaEscritor) > 0 &&
-    strcmp(mensajeRecibido, "Adios\n") != 0){
-    printf("Escritor: %s\n", mensajeRecibido);
-    printf("Lector:");
-    fgets(mensajeEnviado, TAM, stdin);
-    fputs(mensajeEnviado, tuberiaLector);
-    fclose(tuberiaLector);
-    tuberiaLector=fopen("lector","w");
+  if(tuberiaEscritor==NULL || tuberiaLector==NULL){
+      printf("No se pudo abrir las tuberías.\n");
+      exit(0);
   }
 
+  /*
+    El proceso Lector se encarga de leer un mensaje de la
+    tubería y presentarlo en la pantalla, después envía un mensaje
+    introducido, por medio del teclado, al proceso Escritor a través de
+    la otra tubería.
+    Al recibir el mensaje "Adiós\n", termina el proceso.
+  */
+  while(fgets(mensajeRecibido, TAM, tuberiaEscritor) > 0 &&
+         strcmp(mensajeRecibido, "Adiós\n") != 0){
+    /*
+      Obtenemos el mensaje escrito por el proceso Escritor y lo mostramos
+      a pantalla.
+    */
+    printf("Escritor: %s\n", mensajeRecibido);
+    printf("Lector:");
+    /*Enviamos el mensaje ingresado por el teclado al Escritor*/
+    fgets(mensajeEnviado, TAM, stdin);
+    fputs(mensajeEnviado, tuberiaLector);
+    /*
+      Cerramos la tubería para indicar que terminamos de escribir y
+      después la abrimos para volver a escribir en ella.
+    */
+    fclose(tuberiaLector);
+    tuberiaLector=fopen("lector", "w");
+  }
+
+  /*Cerramos la tuberías*/
   fclose(tuberiaEscritor);
   fclose(tuberiaLector);
 }
